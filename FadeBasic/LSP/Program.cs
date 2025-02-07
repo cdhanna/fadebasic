ad75cda65f50147cc1c5a5e31c19995fa8ec56de
Chris Hanna <chris@beamable.com>
2025-02-07 06:28:17 -0500

    more logs on LSP startup

--- FadeBasic/LSP/Program.cs
+++ FadeBasic/LSP/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.IO.Pipes;
+using System.Threading;
 using System.Threading.Tasks;
 using FadeBasic.ApplicationSupport.Project;
 using LSP.Handlers;
@@ -17,98 +18,124 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        var pipeAddr = "";
-        foreach (var t in args)
-        {
-            if (!t.StartsWith("--pipe=")) continue;
-            
-            pipeAddr = t.Substring("--pipe=".Length);
-            break;
-        }
-        
-        Log.Logger = new LoggerConfiguration()
-            .Enrich.FromLogContext()
-            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
-            .WriteTo.Console()
-            .MinimumLevel.Information()
-            .CreateLogger();
-        
-        Log.Logger.Information($"connecting... pipeAddr=[{pipeAddr}]");
-        
-        var pipeClient = new NamedPipeClientStream(pipeAddr);
-        await pipeClient.ConnectAsync();
-        
-        Log.Logger.Information("connected.");
-
         try
         {
-            ProjectLoader.Initialize(); // initialize msbuild.
-            
-            var server = await LanguageServer.From(options =>
-                {
-                    options.OnUnhandledException = ex =>
-                    {
-                        Log.Logger.Information("ERROR YO!!!!" + ex.GetType().Name + " : " + ex.Message + " :: " + ex.StackTrace);
-                    };
-                    options
-                        .WithInput(pipeClient.UsePipeReader())
-                        .WithOutput(pipeClient.UsePipeWriter())
-                        .WithConfigurationSection("conf.language.fade")
-                        .ConfigureLogging(
-                            x => x
-                                .AddSerilog(Log.Logger)
-                                .AddLanguageProtocolLogging()
-                                .SetMinimumLevel(LogLevel.Information)
-                        )
-                        .AddDefaultLoggingProvider()
-                        .WithServices(ConfigureServices)
-                        .WithServices(x => x.AddLogging(b =>
-                        {
-                            b.SetMinimumLevel(LogLevel.Trace);
-                            b.AddSerilog(Log.Logger);
-                            b.AddLanguageProtocolLogging();
-                        }))
-                        .OnInitialize((languageServer, request, token) =>
-                        {
-                            var docService = languageServer.Services.GetService<DocumentService>();
-                            var projectService = languageServer.Services.GetService<ProjectService>();
-                            docService.Populate(request.RootUri);
-                            
-                            foreach (var (uri, text) in docService.AllProjects())
-                            {
-                                projectService.LoadProject(uri);
-                            }
-                            // TODO: populate
-                            // var foo = languageServer.Services.GetService<Foo>();
-                            // foo.SayFoo();
-                            return Task.CompletedTask;
-                        })
-                        
-                        .WithHandler<TextDocumentSyncHandler>()
-                        .WithHandler<ProjectTextDocumentSyncHandler>()
-                        .WithHandler<FormattingHandler>()
-                        .WithHandler<FormattingWhenTypingHandler>()
-                        .WithHandler<FormattingRangeHandler>()
-                        .WithHandler<GotoDefinitionHandler>()
-                        .WithHandler<FindReferencesHandler>()
-                        .WithHandler<HoverHandler>()
-                        // .WithHandler<FoldingRangeHandler>()
-                        // .WithHandler<DocumentSymbolHandler>()
-                        .WithHandler<SemanticTokenHandler>()
-                        // .WithHandler<DiagnosticsHandler>()
-                        .OnStarted((languageServer, token) =>
-                        {
-                            languageServer.Workspace.SendNotification("Derp");
-                            return Task.CompletedTask;
-                        });
-                }
-            ).ConfigureAwait(false);
+            var connectionCts = new CancellationTokenSource();
+            var pipeAddr = "";
+            foreach (var t in args)
+            {
+                if (!t.StartsWith("--pipe=")) continue;
 
-            await server.WaitForExit.ConfigureAwait(false);
+                pipeAddr = t.Substring("--pipe=".Length);
+                break;
+            }
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                .WriteTo.Console()
+                .MinimumLevel.Information()
+                .CreateLogger();
+
+            Log.Logger.Information($"connecting... pipeAddr=[{pipeAddr}]");
+            for (var i = 0 ; i < args.Length; i ++)
+            {
+                Log.Logger.Information($" arg {i}=[{args[i]}]");
+            }
+
+            var pipeClient = new NamedPipeClientStream(pipeAddr);
+            Log.Logger.Information(" created pipe client");
+            try
+            {
+                await pipeClient.ConnectAsync(TimeSpan.FromSeconds(10), connectionCts.Token);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error($"failed to connect to pipe in time. message=[{ex.Message}]");
+                throw;
+            }
+
+            Log.Logger.Information(" connected.");
+
+            try
+            {
+                ProjectLoader.Initialize(); // initialize msbuild.
+                Log.Logger.Information("Creating server");
+
+                var server = await LanguageServer.From(options =>
+                    {
+                        options.OnUnhandledException = ex =>
+                        {
+                            Log.Logger.Information("ERROR YO!!!!" + ex.GetType().Name + " : " + ex.Message +
+                                                   " :: " + ex.StackTrace);
+                        };
+                        options
+                            .WithInput(pipeClient.UsePipeReader())
+                            .WithOutput(pipeClient.UsePipeWriter())
+                            .WithConfigurationSection("conf.language.fade")
+                            .ConfigureLogging(
+                                x => x
+                                    .AddSerilog(Log.Logger)
+                                    .AddLanguageProtocolLogging()
+                                    .SetMinimumLevel(LogLevel.Information)
+                            )
+                            .AddDefaultLoggingProvider()
+                            .WithServices(ConfigureServices)
+                            .WithServices(x => x.AddLogging(b =>
+                            {
+                                b.SetMinimumLevel(LogLevel.Trace);
+                                b.AddSerilog(Log.Logger);
+                                b.AddLanguageProtocolLogging();
+                            }))
+                            .OnInitialize((languageServer, request, token) =>
+                            {
+                                var docService = languageServer.Services.GetService<DocumentService>();
+                                var projectService = languageServer.Services.GetService<ProjectService>();
+                                docService.Populate(request.RootUri);
+
+                                foreach (var (uri, text) in docService.AllProjects())
+                                {
+                                    projectService.LoadProject(uri);
+                                }
+
+                                // TODO: populate
+                                // var foo = languageServer.Services.GetService<Foo>();
+                                // foo.SayFoo();
+                                return Task.CompletedTask;
+                            })
+
+                            .WithHandler<TextDocumentSyncHandler>()
+                            .WithHandler<ProjectTextDocumentSyncHandler>()
+                            .WithHandler<FormattingHandler>()
+                            .WithHandler<FormattingWhenTypingHandler>()
+                            .WithHandler<FormattingRangeHandler>()
+                            .WithHandler<GotoDefinitionHandler>()
+                            .WithHandler<FindReferencesHandler>()
+                            .WithHandler<HoverHandler>()
+                            // .WithHandler<FoldingRangeHandler>()
+                            // .WithHandler<DocumentSymbolHandler>()
+                            .WithHandler<SemanticTokenHandler>()
+                            // .WithHandler<DiagnosticsHandler>()
+                            .OnStarted((languageServer, token) =>
+                            {
+                                languageServer.Workspace.SendNotification("Derp");
+                                return Task.CompletedTask;
+                            });
+                    }
+                ).ConfigureAwait(false);
+
+                Log.Logger.Information(" starting server");
+                await server.WaitForExit.ConfigureAwait(false);
+                Log.Logger.Information(" started server");
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Fatal("Game over: " + ex.GetType().Name + " " + ex.Message + "\n" + ex.StackTrace);
+            }
         }
         catch (Exception ex)
         {
-            Log.Logger.Fatal("Game over: " + ex.GetType().Name + " " + ex.Message + "\n" + ex.StackTrace);
+            Log.Logger.Fatal($"LSP failed. type=[{ex.GetType().Name}] message=[{ex.Message}] stack=[{ex.StackTrace}]");
         }
     }
 
